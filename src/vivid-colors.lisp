4bd1dd62943661237ad04dc0e609a2dd2c70a773
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-08-29 10:53:22 +0900

    Add: VPRINTER object.

--- src/vivid-colors.lisp
+++ src/vivid-colors.lisp
@@ -7,3 +7,12 @@
 (in-package :vivid-colors)
 
 (defparameter *vprint-dispatch* nil)
+
+;;;; VPRINTER
+
+(defstruct vprinter
+  (type (error "TYPE is required.") :read-only t)
+  (function (error "FUNCTION is required.")
+            :type (or function symbol)
+            :read-only t)
+  (priority 0 :type real :read-only t))
